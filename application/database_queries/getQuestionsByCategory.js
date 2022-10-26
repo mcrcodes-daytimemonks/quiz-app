b7a18cd3e79de38c666c090dfeda8f19a2703dcb
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-10-26 13:55:21 +0100

    refactor: Game page
    - Abstracts prisma client to it's own file, so that it can be reused in other database queries
    - Abstracts Game page component out to separate file
    - Refactors getQuestionsByCategory to make it easier to read
    - Adds shuffling of questions and question answers to getQuestionsByCategory
    - Adds limit argument to getQuestionsByCategory

---
+++ application/database_queries/getQuestionsByCategory.js
@@ -0,0 +1,34 @@
+import db from "./databaseClient";
+
+export default async function getQuestionsByCategory({ category, limit }) {
+  try {
+    await db.$connect();
+
+    const questionsByCategory = await db.questions.findMany({
+      where: {
+        tags: {
+          hasEvery: [category],
+        },
+      },
+    });
+
+    await db.$disconnect();
+
+    return JSON.stringify(
+      questionsByCategory
+        .sort((a, b) => Math.random() - 0.5)
+        .slice(0, limit)
+        .map(({ id, question, answers }) =>
+          ({
+            id,
+            question,
+            answers: answers.sort((a, b) => Math.random() - 0.5),
+          })
+        )
+    );
+  } catch (err) {
+    console.error(err);
+    await db.$disconnect();
+    process.exit(1);
+  }
+}
