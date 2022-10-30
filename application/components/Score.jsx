5e57768d48f9b664fd8734247f9f5393b667e451
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-10-30 12:21:22 +0000

    feat: Score component
    Render some of the gameResults data

--- application/components/Score.jsx
+++ application/components/Score.jsx
@@ -1,13 +1,33 @@
 const Score = (props) => {
   const { data } = props;
 
-  const numCorrret = data.filter(item => item.isCorrect).length;
-  
+  const numCorrect = data.filter((item) => item.isCorrect).length;
+
   return (
     <div>
       <h1>Score Component</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <p>You scored {numCorrret}</p>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <p>
+        You scored {numCorrect} out of {data.length}
+      </p>
+      <div>
+        {data.map((result) => (
+          <div
+            key={result.id}
+            style={{
+              padding: "0 2rem",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+          <div>
+            <p>{result.question}</p>
+            {!result.isCorrect && <p> Correct Answer: {result.correctAnswer}</p>}
+          </div>
+            <p>{result.isCorrect ? "tick" : "cross"}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
