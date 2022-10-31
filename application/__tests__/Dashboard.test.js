081d563e3a83b3e67829f0ca8bc14af448ea82ce
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-10-31 22:05:48 +0000

    fix/test: Fix failing tests

--- application/__tests__/Dashboard.test.js
+++ application/__tests__/Dashboard.test.js
@@ -1,12 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import Dashboard from "../components/Dashboard";
 import "@testing-library/jest-dom";
+import { SessionProvider } from "next-auth/react";
 
 const props = {
   handleLogout: jest.fn(),
 };
 
-const renderDashboard = () => render(<Dashboard {...props} />);
+const session = {
+  user: {
+    name: "Michael Shields",
+    email: "mr.m.p.shields@gmail.com",
+    image: "https://avatars.githubusercontent.com/u/73437037?v=4",
+  },
+  expires: "2022-11-30T21:18:08.670Z",
+};
+
+const renderDashboard = () =>
+  render(
+    <SessionProvider session={session}>
+      <Dashboard {...props} />
+      );
+    </SessionProvider>
+  );
 
 describe("Dashboard", () => {
   it("matches snapshot", () => {
@@ -17,7 +33,7 @@ describe("Dashboard", () => {
 
   it("renders a call to action", () => {
     renderDashboard();
-    const callToAction = /please choose a category/i
+    const callToAction = /please choose a category/i;
 
     expect(screen.getByText(callToAction)).toBeInTheDocument();
   });
