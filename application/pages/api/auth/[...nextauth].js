133327e498f51bb95fa9b66567c88c827529dae4
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-11-01 20:04:35 +0000

    feat: Add secret to GithubProvider options

--- application/pages/api/auth/[...nextauth].js
+++ application/pages/api/auth/[...nextauth].js
@@ -4,6 +4,7 @@ import GithubProvider from "next-auth/providers/github";
 const authOptions = {
   providers: [
     GithubProvider({
+      secret: process.env.NEXTAUTH_SECRET,
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
