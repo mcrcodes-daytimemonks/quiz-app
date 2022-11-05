9d8a4a5ccf6adde4b6d0f72c86e26f1af7d4672e
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-11-05 11:05:21 +0000

    refactor: Remove console.log from /pages/getGameResults

--- application/pages/api/getGameResults.js
+++ application/pages/api/getGameResults.js
@@ -1,7 +1,6 @@
 import getGameResults from "../../database_queries/getGameResults";
 
 export default async function handler(req, res) {
-  console.log(req.body);
   const selectedAnswers = JSON.parse(req.body);
   res.status(200).json(await getGameResults(selectedAnswers));
 }
