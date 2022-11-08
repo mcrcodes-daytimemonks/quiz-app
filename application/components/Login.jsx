24a3eeefcec8e354de7e75078cbc3cbee0309f54
Andy Jackson <andypauljackson@gmail.com>
2022-11-08 12:11:55 +0000

    logo added to first page

--- application/components/Login.jsx
+++ application/components/Login.jsx
@@ -1,9 +1,19 @@
 import { signIn } from "next-auth/react";
+import Image from "next/image";
+import { BsPatchQuestionFill } from "react-icons/bs";
 
 const Login = () => {
 
   return (
     <div>
+        <Image
+          alt="manchester codes logo"
+          className="mc-codes logo"
+          height="98px"
+          src="/manchester-codes-logo.svg"
+          width="400vw"
+        />
+      <h1>Manchester Codes Community Quiz App</h1>
       <p>Hey there stranger!
       <br />
       <br />
